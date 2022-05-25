0da0e1a14c468446e59e12c02184b225e60ea764
Samuel Pena <inestbht@gmail.com>
2022-05-24 23:49:24 -0500

    Completed - Ch. 2.9 Loading New Images, An Intro to Types, Type Conversion, and String Interpolation

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,34 +12,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     // This code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
-        let amazingMessage = "You Are Amazing!"
-        let fabulousMessage = "You Are Fabulous!"
-        
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            messageLabel.textColor = UIColor.blue
-            imageView.image = UIImage(named: "image1")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = amazingMessage
-            messageLabel.textColor = UIColor.green
-            imageView.image = UIImage(named: "image2")
-        } else if messageLabel.text == amazingMessage {
-            messageLabel.text = fabulousMessage
-            messageLabel.textColor = UIColor.cyan
-            imageView.image = UIImage(named: "image3")
-        } else {
-            messageLabel.text = awesomeMessage
-            messageLabel.textColor = UIColor.red
-            imageView.image = UIImage(named: "image0")
+
+        print(imageNumber)
+        // let imageName = "image" + String(imageNumber)
+        // String Interpolation
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber = 0
         }
+        
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//        let amazingMessage = "You Are Amazing!"
+//        let fabulousMessage = "You Are Fabulous!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            messageLabel.textColor = UIColor.blue
+//            imageView.image = UIImage(named: "image1")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = amazingMessage
+//            messageLabel.textColor = UIColor.green
+//            imageView.image = UIImage(named: "image2")
+//        } else if messageLabel.text == amazingMessage {
+//            messageLabel.text = fabulousMessage
+//            messageLabel.textColor = UIColor.cyan
+//            imageView.image = UIImage(named: "image3")
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            messageLabel.textColor = UIColor.red
+//            imageView.image = UIImage(named: "image0")
+//        }
     }
     
 }
